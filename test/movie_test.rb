5f6fc86f4fac823a7e7ba979e87f40cf24297ac1
Josh English <josh@joshenglish.com>
2016-05-02 10:21:11 -0500

    Testing Testing Updates

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -8,4 +8,16 @@ describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
   end
+  it "should be Top Gun" do
+    @tg = Movie.new.to_hash
+    @tg["Title"].should == "Top Gun"
+  end
+  it "should be 1986" do
+    @tg = Movie.new.to_hash
+    @tg["Year"].should == "1986"
+  end
+  it "should be PG" do
+    @tg = Movie.new.to_hash
+    @tg["Rated"].should == "PG"
+  end
 end
