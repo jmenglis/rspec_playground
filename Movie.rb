31ed9a82caf26c5cad34547a4482d16ea3d47f07
Josh English <josh@joshenglish.com>
2016-05-02 09:29:01 -0500

    We need to test this

---
+++ Movie.rb
@@ -0,0 +1,16 @@
+class Movie
+
+  def initialize
+    p Rainbow('Movie was instantiated.').cyan
+    @props = HTTParty.get('http://www.omdbapi.com/?t=Top+Gun&y=&plot=short&r=json')
+    puts Rainbow(@props).green
+  end
+
+  def to_hash
+    @props.to_hash
+
+  end
+
+
+
+end
